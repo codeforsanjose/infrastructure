33bdf24340ecdc79d19b4acd9aa2def5256f50b5
Darren Pham <phamdarren1@gmail.com>
2020-12-08 16:20:52 -0800

    remove terragrunt-cache

--- cfc-foodoasis/non-prod/us-east-2/stage/acm/.terragrunt-cache/BHmcp4iYQyEOIHJARaMHizO6al0/mjAaZvf3dcekxywwInydhV6gpeg/acm/variables.tf
+++
@@ -1,8 +0,0 @@
-variable domain_name {
-  description = "The domain name where the application will be deployed, must already live in AWS"
-}
-
-variable subject_alternative_names {
-  description = "Additional subject_alternative_names to be added to the certificate"
-  type = list(string)
-}
--- cfc-foodoasis/non-prod/us-east-2/stage/network/.terragrunt-cache/FGuT8qBzEtjFmvkLsZD2c28Vy94/mjAaZvf3dcekxywwInydhV6gpeg/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "codeforcalifornia"
-    key    = "terraform-state/foodoasis/dev/terraform.tfstate"
-    region = "us-west-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
